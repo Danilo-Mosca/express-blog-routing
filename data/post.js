82438165c588d5cb001b9dd5d1249a9b20daa6d6
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-12-03 16:02:20 +0100

    add express js, images files, and installed module for express js

---
+++ data/post.js
@@ -0,0 +1,41 @@
+// Creo l'array di oggetti con la lista dei post del blog
+const post = [
+    {
+        id: 1,
+        titolo: "Il ciambellone classico: una ricetta semplice e deliziosa",
+        contenuto: "Il ciambellone è un dolce tradizionale italiano che si prepara con pochi ingredienti e si gusta in ogni occasione. Con la nostra ricetta potrai preparare un ciambellone soffice e profumato, perfetto per colazioni e merende.",
+        immagine: "images/ciambellone.jpeg",
+        tags: ["ciambellone", "dolce", "tradizionale", "italiano"]
+    },
+    {
+        id: 2,
+        titolo: "I cracker di barbabietola: un'alternativa salutare e gustosa",
+        contenuto: "I cracker di barbabietola sono un'ottima opzione per chi cerca un'alternativa salutare e gustosa ai classici snack. Con la nostra ricetta potrai preparare dei cracker croccanti e saporiti, perfetti per accompagnare formaggi e verdure.",
+        immagine: "images/cracker_barbabietola.jpeg",
+        tags: ["cracker", "barbabietola", "salutare", "gustoso"]
+    },
+    {
+        id: 3,
+        titolo: "Il pane fritto dolce: un dessert unico e irresistibile",
+        contenuto: "Il pane fritto dolce è un dessert unico e irresistibile che si prepara con pochi ingredienti e si gusta in ogni occasione. Con la nostra ricetta potrai preparare un pane fritto dolce soffice e profumato, perfetto per accompagnare creme e gelati.",
+        immagine: "images/pane_fritto_dolce.jpeg",
+        tags: ["pane fritto", "dolce", "dessert", "unico"]
+    },
+    {
+        id: 4,
+        titolo: "La pasta di barbabietola: un piatto unico e saporito",
+        contenuto: "La pasta di barbabietola è un piatto unico e saporito che si prepara con pochi ingredienti e si gusta in ogni occasione. Con la nostra ricetta potrai preparare una pasta di barbabietola cremosa e profumata, perfetta per accompagnare carni e verdure.",
+        immagine: "images/pasta_barbabietola.jpeg",
+        tags: ["pasta", "barbabietola", "unico", "saporito"]
+    },
+    {
+        id: 5,
+        titolo: "La torta paesana: un dolce tradizionale e delizioso",
+        contenuto: "La torta paesana è un dolce tradizionale e delizioso che si prepara con pochi ingredienti e si gusta in ogni occasione. Con la nostra ricetta potrai preparare una torta paesana soffice e profumata, perfetta per colazioni e merende.",
+        immagine: "images/torta_paesana.jpeg",
+        tags: ["torta", "paesana", "dolce", "tradizionale"]
+    }
+];
+
+// Esporto l'oggetto posts
+module.exports = post;
